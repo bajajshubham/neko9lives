4386f4d3b25738ac91eb76f5533d73f7120ba6cc
Shubham Bajaj <77834591+bajajshubham@users.noreply.github.com>
2023-08-03 14:06:26 +0000

    Created join room UI page

--- src/components/HandleRoutes.js
+++ src/components/HandleRoutes.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
 import MainMenu from "./menu/MainMenu";
+import Join from "./room/Join/Join";
 
 const HandleRoutes = () => {
   return (
@@ -9,6 +10,7 @@ const HandleRoutes = () => {
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<MainMenu />} />
+      <Route path="/join" element={<Join />} />
 
       <Route path="*" element={<p>There's nothing here: 404!</p>} />
     </Routes>
