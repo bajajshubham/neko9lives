e4bcd1e6771aa1a9373905de126ad06492c5ad28
Shubham Bajaj <77834591+bajajshubham@users.noreply.github.com>
2023-08-03 14:46:29 +0000

    Added helper components

--- src/components/HandleRoutes.js
+++ src/components/HandleRoutes.js
@@ -1,20 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
+import Register from "./auth/Register";
 import MainMenu from "./menu/MainMenu";
 import Join from "./room/Join/Join";
 import Create from "./room/Create/Create";
 import Lobby from "./room/Create/Lobby";
+import Playground from "./room/Playground/Playground.js";
 
 const HandleRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
       <Route path="/home" element={<MainMenu />} />
       <Route path="/join" element={<Join />} />
       <Route path="/room/create" element={<Create />} />
       <Route path="/lobby/:roomID" element={<Lobby />} />
+      {/* <Route path="/playground/:roomID" element={<Playground />} /> */}
 
       <Route path="*" element={<p>There's nothing here: 404!</p>} />
     </Routes>
