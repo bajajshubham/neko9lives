b05a92e09bfaf0831c0514dad0d0d4d3c0437c45
Shubham Bajaj <77834591+bajajshubham@users.noreply.github.com>
2023-08-03 13:57:23 +0000

    Added main menu UI page

--- src/components/HandleRoutes.js
+++ src/components/HandleRoutes.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./auth/Login";
+import MainMenu from "./menu/MainMenu";
 
 const HandleRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
+      <Route path="/home" element={<MainMenu />} />
 
       <Route path="*" element={<p>There's nothing here: 404!</p>} />
     </Routes>
